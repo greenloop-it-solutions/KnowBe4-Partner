a1e7264c71ce3bf95ff13986c6d0c49018ae38e4
smood922 <48338396+smood922@users.noreply.github.com>
2023-11-06 14:01:15 -0800

    Update Invoke-KB4CallBack.ps1
    Updated to get callback URL dynamically from SAMLResponse

--- Private/Invoke/Invoke-KB4CallBack.ps1
+++ Private/Invoke/Invoke-KB4CallBack.ps1
@@ -13,8 +13,32 @@ function Invoke-KB4CallBack {
     $headers.Add("Content-Type", "application/x-www-form-urlencoded")
     $body = $SAMLResponse
 
+    # SAML Response in Base64
+    $base64SamlResponse = [System.Web.HttpUtility]::UrlDecode($samlresponse)
+    # Remove the 'samlresponse=' part to get the pure Base64 string
+    $base64SamlResponse = $base64SamlResponse -replace '^samlresponse=', ''
+    # Base64 decode the SAML response
+    $decodedBytes = [System.Convert]::FromBase64String($base64SamlResponse)
+
+    try {
+        # Assume the SAML response is deflated. Try to inflate (unzip) it.
+        $ms = New-Object System.IO.MemoryStream $decodedBytes
+        $deflateStream = New-Object System.IO.Compression.DeflateStream $ms, ([System.IO.Compression.CompressionMode]::Decompress)
+        $sr = New-Object System.IO.StreamReader $deflateStream
+        $samlXml = $sr.ReadToEnd()
+    } catch {
+        # If the inflate operation fails, the SAML response might not be deflated.
+        # In this case, just convert the decoded bytes to a string.
+        $samlXml = [Text.Encoding]::UTF8.GetString($decodedBytes)
+    }
+
+    # Assume $samlXml contains the SAML response XML content
+    # Convert the SAML XML string to an XML document object
+    $xmlDoc = [xml]$samlXml
+    $destination = $xmlDoc.Response.destination
+
     $callBackParams = @{
-        Uri     = 'https://training.knowbe4.com/reseller/auth/saml/ef1b1febd1bf/callback'
+        Uri     = "$destination"
         Method  = 'Post'
         Headers = $headers
         Body    = $body
